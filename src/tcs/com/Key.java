a42acfc7d4dac636f5f5a497139e83fc3084e994
kamesh79 <kameshking79@gmail.com>
2021-12-31 16:47:25 +0530

    Password functionalities

--- src/tcs/com/Key.java
+++ src/tcs/com/Key.java
@@ -2,10 +2,13 @@ package tcs.com;
 
 public class Key {
 	
+	private static  void v2() {
+		
+		System.out.println("B");
+	}
+	
 	public static void main(String[] args) {
 		
-		System.out.println("Test");
-		
+		v2();	
 	}
-
 }
