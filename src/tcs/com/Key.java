b50b714c6a7764283ae6bef86a4fd6fdf349d403
kamesh79 <kameshking79@gmail.com>
2021-12-31 16:27:11 +0530

    Username

--- src/tcs/com/Key.java
+++ src/tcs/com/Key.java
@@ -2,9 +2,14 @@ package tcs.com;
 
 public class Key {
 	
+	private static void v1() {
+
+		System.out.println("A");
+	}
+	
 	public static void main(String[] args) {
 		
-		System.out.println("Test");
+		v1();
 		
 	}
 
